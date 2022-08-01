66d6009b0fe9a0e7750d50cba2c9b08476ca27ab
Benjamin Engen <ben@constellationpolitical.com>
2022-08-01 09:09:11 +0000

    fix: switch sql declares to jinja

--- models/intermediate/vote-history/int_co_22g_turnout_probability.sql
+++ models/intermediate/vote-history/int_co_22g_turnout_probability.sql
@@ -8,15 +8,14 @@
     materialized="table"
 )}}
 
-DECLARE election_lst ARRAY <DATE>;
-DECLARE max_yr INT64;
 
 /*
     The array election_lst must be populated manually with the dates of the last four General Elections
     preceding the election of interest. The current configuration is for the 2022 Colorado General.
 */
-SET election_lst = [DATE('2014-11-04'), DATE('2016-11-08'), DATE('2018-11-06'), DATE('2020-11-03')];
-SET max_yr = EXTRACT(YEAR FROM election_lst[offset(3)]);
+
+{% set election_lst = ['2014-11-04', '2016-11-08', '2018-11-06', '2020-11-03'] %}
+{% set max_yr = '2020' %}
 
 
 WITH vote_history AS(
@@ -31,7 +30,7 @@ binary_votes AS (
         election_date,
         COUNT(voter_id) AS voted
     FROM vote_history
-    WHERE election_date IN UNNEST(election_lst)
+    WHERE CAST(election_date AS STRING) IN UNNEST( {{election_lst}} )
     GROUP BY voter_id, election_date
     ORDER BY voter_id, election_date ASC
 ),
@@ -40,14 +39,14 @@ vote_eligibility AS (
     SELECT
         voter_id,
         voted,
-        DIV(((max_yr + 2) - EXTRACT(YEAR FROM FIRST_VALUE(ElECTION_DATE) OVER(PARTITION BY VOTER_ID ORDER BY ElECTION_DATE ASC))), 2) AS eligibility
+        DIV((({{max_yr}} + 2) - EXTRACT(YEAR FROM FIRST_VALUE(election_date) OVER(PARTITION BY voter_id ORDER BY election_date ASC))), 2) AS eligibility
     FROM binary_votes
 ),
 
 vote_probability AS (
     SELECT
         voter_id,
-        SAFE_DIVIDE(SUM(VOTED), eligibility) AS probability
+        SAFE_DIVIDE(SUM(voted), eligibility) AS probability
     FROM vote_eligibility
     GROUP BY voter_id, eligibility
     ORDER BY voter_id ASC
@@ -58,4 +57,3 @@ SELECT
     *
 FROM vote_probability
 ORDER BY voter_id ASC
-LIMIT 10
